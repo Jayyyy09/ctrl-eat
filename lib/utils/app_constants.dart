091a3fabe207b67a2a098f0ba9edb3a8d2976a19
Jayyyy09 <ajlagat12@gmail.com>
2023-04-22 12:12:14 +0800

    Co-authored-by: lei <appleeemily@users.noreply.github.com>

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,1 +1,10 @@
 
+class AppConstants{
+    static const String APP NAME = "CTRL + Eat";
+    static const int APP_VERSION = 1;
+
+    static const String BASE URL = "https://net950.wordpress.com/sample-page/"; //url din
+    static const String INDEMAND_PRODUCT_URI = "";//api directory
+    static const String RECOMMENDED_PRODUCT_URI = "";//api directory
+
+}
